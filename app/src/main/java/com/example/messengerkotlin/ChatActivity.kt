f30cfb94640429d032a21ca7fab1242631176916
Artanov Nikolai <merlandodeluxe@gmail.com>
2024-07-14 12:00:04 +0500

    1.0

--- app/src/main/java/com/example/messengerkotlin/ChatActivity.kt
+++ app/src/main/java/com/example/messengerkotlin/ChatActivity.kt
@@ -1,12 +1,22 @@
 package com.example.messengerkotlin
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var textViewTitle: TextView;
+    private lateinit var imageViewUserStatus: ImageView;
+    private lateinit var recycleViewMessages: RecyclerView;
+    private lateinit var editTextMessage: EditText;
+    private lateinit var imageViewSendMessage: ImageView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,14 @@ class ChatActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initializeAllElements()
+    }
+
+    private fun initializeAllElements() {
+        textViewTitle = findViewById(R.id.textViewTitle);
+        imageViewUserStatus = findViewById(R.id.imageViewUserStatus);
+        recycleViewMessages = findViewById(R.id.recycleViewMessages);
+        editTextMessage = findViewById(R.id.editTextMessage);
+        imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
     }
 }
